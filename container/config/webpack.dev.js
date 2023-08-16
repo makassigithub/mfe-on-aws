cbba7a221e3f137a9e09366c4a31d52884cc5a39
Brahima Traore <makassi@BrahimaMac.local>
2023-08-16 19:18:32 -0400

    marketing integration to container

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,23 +1,24 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPluging = require('webpack/lib/container/ModuleFederationPlugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
 
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
     mode: 'development',
     devServer:{
-        port: 8081,
+        port: 8080,
         historyApiFallback:{
             index:'index.html'
         }
     },
     plugins:[
-        new ModuleFederationPluging({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes:{
-                './MarketingApp': './src/bootstrap.js'
+        new ModuleFederationPlugin({
+            name: 'container',
+            filename:'remoteEntry.js',
+            remotes:{
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             }
         }),
         new HtmlWebpackPlugin({
---
+++ container/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/marketing';
+
+
+
+export default () =>
+    <div>
+        <h1>Inside container</h1>
+        <hr/>
+        <MarketingApp/>
+    </div> 
+
