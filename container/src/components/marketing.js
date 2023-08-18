3d795d8ae21f8902ce4c54648448b8bbce91e1dc
Brahima Traore <makassi@BrahimaMac.local>
2023-08-18 18:29:06 -0400

    nagivation between host and child

--- container/src/components/marketing.js
+++ container/src/components/marketing.js
@@ -1,13 +1,32 @@
 import React, { useEffect, useRef} from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { mount } from "marketing/MarketingApp";
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory()
 
     useEffect(()=>{
-        console.log(ref.current);
-        mount(ref.current);
-    })
+       const { onParentNavigate } =  mount(ref.current, 
+            {  
+                // when history.listen is called inside the container,
+                // It passes the location object to the provided callback
+                // We can use the <pathname> of that object
+                onNavigate :({ pathname: nextPathName }) => {
+
+                    // We now update the browser url with the incomin path
+                    // But only if it is different the host current path
+                    const { pathname }  = history.location;
+                    if(pathname !== nextPathName){
+                        history.push(nextPathName);
+                    }
+                        
+                }
+            });
+
+            history.listen(onParentNavigate);
+    },[])
 
     return <div ref={ref}></div>
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'
 
 import App from './app';
 
-
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    //Default history is only provided in development mode
+    const history = defaultHistory || createMemoryHistory();
     ReactDOM.render(
-        <App/>, el
+        <App history={history}/>, el
     )
+    // The history.listen() receives a callback and calls it by providing 
+    // the location object to it when the history changes
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+        
+        
+        // We also want the navigate function to return some information 
+        //after it is called by the parent that can be used by history.listen to update
+        // child's history too.
+        return {
+            onParentNavigate:({ pathname: nextPathName} )=> {
+                const {pathname} = history.location;
+                if(nextPathName !== pathname){
+                    history.push(nextPathName)
+                }
+            }
+        }
+
 };
 
 if(process.env.NODE_ENV === 'development'){
     const root = document.querySelector('#_dev_marketing_root');
     if(root){
-        mount(root);
+        mount(root,{defaultHistory: createBrowserHistory()});
     }
 }
 
 
-export { mount};
+export { mount };
