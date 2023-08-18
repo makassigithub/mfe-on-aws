e063e74baf4a0bbdd82dd3d82277a5e87eef4a95
Brahima Traore <makassi@BrahimaMac.local>
2023-08-18 01:25:00 -0400

    customise production css classNames

--- container/src/app.js
+++ container/src/app.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 import Header from './components/Header'
 import MarketingApp from './components/marketing';
 
+const generateClassname = createGenerateClassName({
+    productionPrefix: 'co'
+})
+
 export default () =>
 
 <BrowserRouter>
-    <div>
-        <Header/>
-        <MarketingApp/>
-    </div> 
+    <StylesProvider generateClassName={generateClassname}>
+        <div>
+            <Header/>
+            <MarketingApp/>
+        </div> 
+    </StylesProvider>
 </BrowserRouter>
 
 
