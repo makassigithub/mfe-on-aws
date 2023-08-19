52de880368f2c0831341b3531d312f3ef6fa37b6
Brahima Traore <makassi@BrahimaMac.local>
2023-08-18 22:58:57 -0400

    lazy load child apps

--- container/src/app.js
+++ container/src/app.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
+import Header from './components/Header';
+import Progress from './components/Progress';
 
-import Header from './components/Header'
-import MarketingApp from './components/marketing';
-import AuthApp from './components/auth';
+const LazyAuth = lazy(()=> import('./components/auth'));
+const LazyMarketing = lazy(()=> import('./components/marketing'));
 
 const generateClassname = createGenerateClassName({
     productionPrefix: 'co'
@@ -16,10 +17,12 @@ export default () =>
     <StylesProvider generateClassName={generateClassname}>
         <div>
            <Header/>
-            <Switch>
-                <Route path='/auth' component={AuthApp}></Route>
-                <Route path='/' component={MarketingApp}></Route>
+           <Suspense fallback={<Progress/>}>
+           <Switch>
+                <Route path='/auth' component={LazyAuth}></Route>
+                <Route path='/' component={LazyMarketing}></Route>
             </Switch>
+           </Suspense>
         </div> 
     </StylesProvider>
 </BrowserRouter>
